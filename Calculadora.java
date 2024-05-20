c90960eaf604b786b393f95aaf009b97810ac947
Pamela De Moura <168426031+Porfhirius@users.noreply.github.com>
2024-05-20 08:18:34 -0300

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,19 @@
+package aulaExtra;
+
+public class calculadora {
+	public int somar(int a,int b) {
+		return a + b;
+}
+
+	public int subtrair(int a,int b) {
+		return a - b;
+	}
+	
+	public int multiplicar(int a,int b) {
+		return a * b;
+	}
+	
+	public int dividir(int a,int b) {
+		return a / b;
+	}
+}
