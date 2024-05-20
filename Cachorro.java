d74e81c59875652a0a77479c7bba676292fa1911
Pamela De Moura <168426031+Porfhirius@users.noreply.github.com>
2024-05-20 08:04:23 -0300

    Create Cachorro.java

---
+++ Cachorro.java
@@ -0,0 +1,44 @@
+package aulaExtra;
+
+public class Cachorro {
+	private String nome;
+	private String raca;
+	private int idade;
+	
+	public Cachorro(String novoNome,String novaRaca, int novaIdade) {
+		this.nome = novoNome;
+		this.raca = novaRaca;
+		this.idade = novaIdade;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getRaca() {
+		return raca;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	
+	public void setRaca(String novoRaca) {
+		this.raca = novoRaca;
+	}
+	
+	public void setIdade(int novaIdade) {
+		this.idade = novaIdade;
+	}
+	
+	public String latir () {
+		return "Au au!";
+	
+	}
+	
+	
+}
