76856746b40289008f652711b5d8f2ffe4f41a34
Pamela De Moura <168426031+Porfhirius@users.noreply.github.com>
2024-05-20 08:24:02 -0300

    Create Data.java

---
+++ Data.java
@@ -0,0 +1,60 @@
+package aulaExtra;
+
+public class Data {
+	public int dia;
+	public int mes;
+	public int ano;
+	
+	public Data(int novoDia, int novoMes, int novoAno) {
+		this.dia = novoDia;
+		this.mes = novoMes;
+		this.ano = novoAno;
+	}
+	
+	public int getDia() {
+		return dia;
+	}
+	
+	public int getMes() {
+		return mes;
+	}
+	
+	public int getAno() {
+		return ano;
+	}
+	
+	public void setDia(int novoDia) {
+		this.dia = novoDia;
+	}
+	
+	public void setMes(int novoMes) {
+		this.mes = novoMes;
+	}
+	
+	public void setAno(int novoAno) {
+		this.ano = novoAno;
+	}
+	
+	public String toString() {
+		return "[" + this.dia + "]" + "[" + this.mes + "]" + "[" + this.ano + "]";
+		
+	
+	
+	
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
